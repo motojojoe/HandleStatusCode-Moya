ee41fd7202708c185c2e4fda923afff0ba5af637
Atthachai Meethong <atthachai.mee@gmail.com>
2018-08-04 14:30:46 +0700

    Add all files

---
+++ HandleStatusCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandleStatusCode
+//
+//  Created by Atthachai Meethong on 8/4/18.
+//  Copyright © 2018 Atthachai Meethong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
