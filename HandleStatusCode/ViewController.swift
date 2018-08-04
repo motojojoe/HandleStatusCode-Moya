f64b138d708d930ac0468a6b83a955932075b85b
Atthachai Meethong <atthachai.mee@gmail.com>
2018-08-04 14:59:14 +0700

    Add UI send request

--- HandleStatusCode/ViewController.swift
+++ HandleStatusCode/ViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sendRequest(_ sender: Any) {
+        
     }
-
 
 }
 
